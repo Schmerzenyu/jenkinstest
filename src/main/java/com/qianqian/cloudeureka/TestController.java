e680c66404d1300cea8874dbd478293f095ead79
yuzhengwei <yuzhengwei@abupdate.com>
2020-09-06 13:36:56 +0800

    jenkins

---
+++ src/main/java/com/qianqian/cloudeureka/TestController.java
@@ -0,0 +1,24 @@
+package com.qianqian.cloudeureka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    private MqttGateway mqttGateway;
+
+    @RequestMapping("/sendMqtt.do")
+    public String sendMqtt(String  sendData){
+        mqttGateway.sendToMqtt(sendData,"hello");
+        return "OK";
+    }
+
+    @RequestMapping("/jenkins")
+    public String jenkins(){
+        return "jenkins complete";
+    }
+}
